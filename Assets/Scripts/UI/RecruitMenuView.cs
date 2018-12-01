e296a196ec4f18448c310f4fd0d94d9bca79250e
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-01 20:15:11 -0200

    Close Recruit windows when there's no more villagers to recruit

--- Assets/Scripts/UI/RecruitMenuView.cs
+++ Assets/Scripts/UI/RecruitMenuView.cs
@@ -11,11 +11,14 @@ public class RecruitMenuView : MonoBehaviour
     [SerializeField]
     private GameObject m_VillagerViewPrefab;
 
+    private int m_Count;
+
     public event Action<Villager> OnRecruit;
     public event Action OnRecruitFulfill;
 
     public void AddVillagerView(Villager villager)
     {
+        m_Count++;
         VillagerView villagerView = Instantiate(m_VillagerViewPrefab, Content).GetComponent<VillagerView>();
         villagerView.SetStats((int)villager.WoodProduction, (int)villager.FoodProduction, (int)villager.FaithProduction);
         villagerView.OnActionButton += (i) => { Recruit(villager); };
@@ -30,6 +33,10 @@ public class RecruitMenuView : MonoBehaviour
     void Recruit(Villager villager)
     {
         OnRecruit?.Invoke(villager);
+        m_Count--;
+
+        if (m_Count == 0)
+            CloseView();
     }
 
     public void CloseView()
