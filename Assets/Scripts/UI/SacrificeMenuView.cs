def95818787afaada5aa4a97388ddfa9e5e84c3d
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-01 17:16:00 -0200

    Add SacrificeMenuView and interface for adding villager views

---
+++ Assets/Scripts/UI/SacrificeMenuView.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SacrificeMenuView : MonoBehaviour
+{
+    public Text SacrificeCountText;
+    public Transform Content;
+
+    [SerializeField]
+    private GameObject m_VillagerViewPrefab;
+
+    private int m_CurrentSacrificeCount;
+    private int m_SacrificeCount;
+
+    public event Action<int> OnSacrifice;
+    public event Action OnSacrificeFulfill;
+
+    void AddVillagerView(Villager villager, int index)
+    {
+        VillagerView villagerView = Instantiate(m_VillagerViewPrefab, Content).GetComponent<VillagerView>();
+        villagerView.SetStats((int)villager.WoodProduction, (int)villager.FoodProduction, (int)villager.FaithProduction);
+        villagerView.Index = index;
+        villagerView.OnActionButton += Sacrifice;
+    }
+
+    void SetSacrificeCount(int count)
+    {
+        m_CurrentSacrificeCount = 0;
+        m_SacrificeCount = count;
+    }
+
+    void Sacrifice(int index)
+    {
+        m_CurrentSacrificeCount++;
+        OnSacrifice?.Invoke(index);
+
+        if (m_CurrentSacrificeCount == m_SacrificeCount)
+            OnSacrificeFulfill?.Invoke();
+    }
+
+    public void CloseView()
+    {
+        OnSacrificeFulfill?.Invoke();
+    }
+}
---
+++ Assets/Scripts/UI/VillagerView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class VillagerView : MonoBehaviour
+{
+    public Text WoodText;
+    public Text FoodText;
+    public Text FaithText;
+
+    public int Index;
+
+    public event Action<int> OnActionButton;
+
+    public void SetStats(int wood, int food, int faith)
+    {
+        WoodText.text = wood.ToString();
+        FoodText.text = food.ToString();
+        FaithText.text = faith.ToString();
+    }
+
+    public void Sacrifice()
+    {
+        OnActionButton?.Invoke(Index);
+    }
+}
