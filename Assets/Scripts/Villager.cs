09c1eaede2787caf1ddc13d1fec34b10d9867523
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-01 13:42:45 -0200

    Add Villager data class

---
+++ Assets/Scripts/Villager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Villager
+{
+    public float WoodProduction;
+    public float FoodProduction;
+    public float FaithProduction;
+}
