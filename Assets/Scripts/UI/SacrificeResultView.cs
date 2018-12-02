768faba2f9ae835f149945839ee6c478a19b8c17
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-01 23:02:05 -0200

    Add SacrificeResultView

---
+++ Assets/Scripts/UI/SacrificeResultView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SacrificeResultView : MonoBehaviour
+{
+    public Text Title;
+    public Image Icon;
+    public Text Description;
+
+    public event Action OnWindowClose;
+
+    public void Initialize(string title, Image icon, string text)
+    {
+        Title.text = title;
+        Icon = icon;
+        Description.text = text;
+    }
+
+    public void CloseView()
+    {
+        OnWindowClose?.Invoke();
+        Destroy(this.gameObject);
+    }
+}
