d2bf7ac419da7a4b7beacecd1a4a7cae1e8eb1c2
Milena Mariz <milenaferreira294@hotmail.com>
2018-12-02 17:01:11 -0200

    Add interal path control

--- Assets/Scripts/ControlMovement.cs
+++ Assets/Scripts/ControlMovement.cs
@@ -5,38 +5,36 @@ using Cinemachine;
 
 public class ControlMovement : MonoBehaviour {
 
-	public float m_StartPoint = 1f;
-	public float m_EndPoint = 34f;
+	public GameObject ObjectPath;
 
 	private CinemachineDollyCart m_PathMovement;
+	private CinemachineSmoothPath m_SmoothPath;
 
 	private bool foward = true;
 
 	// Use this for initialization
 	void Start () {
 		m_PathMovement = this.GetComponent<CinemachineDollyCart>();
-		m_PathMovement.m_Position = m_StartPoint;
+		m_SmoothPath =  ObjectPath.GetComponent<CinemachineSmoothPath>();
+
+		m_PathMovement.m_Position = m_SmoothPath.MinPos;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (foward) {
-			if (m_PathMovement.m_Position <= m_EndPoint) {
-				m_PathMovement.m_Position += 0.1f;
-				Debug.Log("Moving foward, currently at: " + m_PathMovement.m_Position);
+			if (m_PathMovement.m_Position <= m_SmoothPath.MaxPos - 0.01f ) {
+				m_PathMovement.m_Position += 0.01f;
 			}
 			else {
-				Debug.Log("Arrived at the end point!");
 				foward = false;
 			}
 		}
 		else {
-			if (m_PathMovement.m_Position >= m_StartPoint) {
-				m_PathMovement.m_Position -= 0.1f;
-				Debug.Log("Moving backward, currently at: " + m_PathMovement.m_Position);
+			if (m_PathMovement.m_Position >= m_SmoothPath.MinPos + 0.01f) {
+				m_PathMovement.m_Position -= 0.01f;
 			}
 			else {
-				Debug.Log("Arrive at the start point!");
 				foward = true;
 			}
 		}
