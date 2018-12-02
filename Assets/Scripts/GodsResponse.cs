334b2fbe7653663f704cac18fae1320c87c2576f
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-01 22:12:00 -0200

    Create God's Response scriptable object

---
+++ Assets/Scripts/GodsResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public struct ResourceModifier
+{
+    public ResourceType Type;
+    public int Amount;
+}
+
+[CreateAssetMenu(menuName = "Village Gods/Create God's Response")]
+public class GodsResponse : ScriptableObject
+{
+    public string Title;
+    public Image Icon;
+    public string Text;
+    public ResourceModifier Modifier;
+}
