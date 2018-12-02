9d0370c08042ed685d012043e73002ca68ccba2a
Milena Mariz <milenaferreira294@hotmail.com>
2018-12-01 23:00:23 -0200

    Create SacrificeOutcome managing class
    Class will be responsible to computate and modify the game accord to the sacrifice outcome.

---
+++ Assets/Scripts/SacrificeOutcome.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SacrificeOutcome : MonoBehaviour {
+
+	public List<SacrificeResult> FoodPositiveEffects;
+	public List<SacrificeResult> FoodNegativeEffects;
+	public List<SacrificeResult> WoodPositiveEffects;
+	public List<SacrificeResult> WoodNegativeEffects;
+	public List<SacrificeResult> FaithPositiveEffects;
+	public List<SacrificeResult> FaithNegativeEffects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
