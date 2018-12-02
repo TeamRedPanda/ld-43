ca8b22bc3ed3b7287c589a720c6529355c34c393
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-02 10:02:42 -0200

    Add GameOver UI support

---
+++ Assets/Scripts/UI/GameOverView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverView : MonoBehaviour
+{
+    public Text FlavourText;
+    public List<string> FlavourTexts;
+
+    // Use this for initialization
+    void Start()
+    {
+        int index = Random.Range(0, FlavourTexts.Count);
+        FlavourText.text = FlavourTexts[index];
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
