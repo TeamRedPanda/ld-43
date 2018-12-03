1201054a2daa11422200e8c144b5931b5bb3e43f
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-03 11:34:59 -0200

    Add villager spawn points

---
+++ Assets/Scripts/VillagerGraphics.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerGraphics : MonoBehaviour
+{
+    public GameObject[] VillagerPrefabs;
+
+    [SerializeField]
+    private List<GameObject> EmptyPoints;
+    private List<GameObject> UsedPoints;
+
+    // Use this for initialization
+    void Start()
+    {
+        UsedPoints = new List<GameObject>();
+
+        foreach(var point in EmptyPoints)
+        {
+            point.GetComponentInChildren<MeshRenderer>().enabled = false;
+        }
+    }
+
+    public void Spawn()
+    {
+        int index = Random.Range(0, EmptyPoints.Count);
+    }
+
+    public void Despawn()
+    {
+        int index = Random.Range(0, EmptyPoints.Count);
+    }
+}
