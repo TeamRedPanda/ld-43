877997573cb737196bbe076add55eb6e0864ed00
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-03 10:20:33 -0200

    Add over population window and view class

---
+++ Assets/Scripts/UI/OverPopulationView.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class OverPopulationView : MonoBehaviour
+{
+    public int Count {
+        set {
+            UpdateFlavourText(value);
+        }
+    }
+
+    public Text FlavourText;
+
+    public List<string> FlavourTexts;
+
+    public event Action OnWindowClose;
+
+    void UpdateFlavourText(int count)
+    {
+        int index = UnityEngine.Random.Range(0, FlavourTexts.Count);
+        FlavourText.text = string.Format(FlavourTexts[index], count);
+    }
+
+    public void CloseView()
+    {
+        OnWindowClose?.Invoke();
+        Destroy(this.gameObject);
+    }
+}
