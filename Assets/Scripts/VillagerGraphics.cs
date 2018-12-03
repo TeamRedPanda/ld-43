2bdd515476202a998a0f182a85b9a5f7e4a9aa89
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-03 11:48:54 -0200

    Add villager graphic prefab

--- Assets/Scripts/VillagerGraphics.cs
+++ Assets/Scripts/VillagerGraphics.cs
@@ -24,10 +24,28 @@ public class VillagerGraphics : MonoBehaviour
     public void Spawn()
     {
         int index = Random.Range(0, EmptyPoints.Count);
+        GameObject parent = EmptyPoints[index];
+        EmptyPoints.RemoveAt(index);
+        UsedPoints.Add(parent);
+
+        int prefabIndex = Random.Range(0, VillagerPrefabs.Length);
+        GameObject prefab = VillagerPrefabs[prefabIndex];
+
+        Quaternion rotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+        Instantiate(prefab, Vector3.zero, rotation, parent.transform);
     }
 
     public void Despawn()
     {
-        int index = Random.Range(0, EmptyPoints.Count);
+        int index = Random.Range(0, UsedPoints.Count);
+
+        // Remove the villager
+        foreach(Transform child in UsedPoints[index].transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        EmptyPoints.Add(UsedPoints[index]);
+        UsedPoints.RemoveAt(index);
     }
 }
